6cae995656bb614b3752d1deb2dd3032ff5a8fea
Julio Capote <julio@twitter.com>
2012-06-02 22:34:21 -0700

    works!

--- src/main/scala/com/capotej/ghettogram/App.scala
+++ src/main/scala/com/capotej/ghettogram/App.scala
@@ -4,12 +4,20 @@ import com.posterous.finatra._
 
 object App {
 
+  case class ImageViewer(var original: String)
+
   class GhettoGram extends FinatraApp{
+
+
+    get("/") { r =>
+      val img = new ImageViewer("http://i.imgur.com/jXHX1.jpg")
+      render(path="index.mustache", exports=img)
+    }
 
     // get /img?src=http://www.example.com/a.jpg
     get("/img") { r =>
       r.params.get("src") match {
-        case Some(src) => rawResponse(body=ProcessImage(src))
+        case Some(src) => rawResponse(body=ProcessImage(src), headers=Map("Content-Type" -> "image/jpeg"))
         case None => response(status=500, body="please supply an src param")
       }
     }
--- src/main/scala/com/capotej/ghettogram/ProcessImage.scala
+++ src/main/scala/com/capotej/ghettogram/ProcessImage.scala
@@ -1,20 +1,56 @@
 package com.capotej.ghettogram
 
+import org.apache.commons.exec._
+import java.io._
+
 object ProcessImage {
 
   def apply(src:String) = {
     val pi = new ProcessImage
-    pi.src = src
+    pi.src = Some(src)
     pi.bytes
   }
 
 }
 
+
 class ProcessImage {
-  var src:String = null
+
+  var inputPath:Option[String] = None
+  var outputPath:Option[String] = None
+  var src:Option[String] = None
+
+  def rand = {
+    Math.abs(scala.util.Random.nextInt)
+  }
+
+  def cmd(str: String) = {
+    val cmdline = CommandLine.parse(str)
+    val executor = new DefaultExecutor
+    executor.setWorkingDirectory(new java.io.File("/tmp"))
+    executor.execute(cmdline)
+  }
+
+  def contrast {
+    inputPath foreach { path =>
+      outputPath = Some("/tmp/" + rand)
+      println("outputing to " + outputPath.get)
+      cmd("convert " + path + " -sigmoidal-contrast 4,0% " + outputPath.get)
+    }
+  }
+
+  def download {
+    src foreach { url =>
+      inputPath = Some("/tmp/" + rand)
+      cmd("curl -o " + inputPath.get + " " + url)
+    }
+  }
 
   def bytes = {
-    "lol".getBytes
+    download
+    contrast
+    val fis = new FileInputStream(outputPath.get)
+    Stream.continually(fis.read).takeWhile(-1 !=).map(_.toByte).toArray
   }
 
 }
